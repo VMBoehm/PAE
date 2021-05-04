6629a635333bb8bdfad1a482e2a49666ce7293ab
Vanessa Boehm <boehmvanessa@gmail.com>
2021-05-04 16:01:59 -0700

    OoD test done, sigma plots, before starting flow extension

---
+++ pae/flow.py
@@ -0,0 +1,136 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+import tensorflow_hub as hub
+tfd = tfp.distributions
+tfb = tfp.bijectors
+import numpy as np
+
+class SplineParams(tf.Module):
+
+    def __init__(self, nunits, nbins=36):
+        self._nbins = nbins
+        self._nunits= nunits
+        self._built = False
+        self._bin_widths = None
+        self._bin_heights = None
+        self._knot_slopes = None
+
+    def _bin_positions(self, x):
+        x = tf.reshape(x, [-1,self._nunits,self._nbins])
+        net = tf.layers.Dense(self._nunits)(x)
+        net = tf.nn.leaky_relu(net)
+        net = tf.layers.Dense(self._nunits)(net)
+        net = tf.nn.leaky_relu(net)
+        net = tf.layers.Dense(self._nbins)(net)
+        net = tf.reshape(net, [-1,self._nunits, self._nbins])
+        return tf.math.softmax(net, axis=-1) * (2 - self._nbins * 1e-2) + 1e-2
+
+    def _slopes(self, x):
+        x = tf.reshape(x, [-1,  self._nunits, ( self._nbins - 1)])
+        net = tf.layers.Dense(self._nunits)(x)         
+        net = tf.nn.leaky_relu(net)
+        net = tf.layers.Dense(self._nbins-1)(net)
+        net = tf.reshape(net, [-1,  self._nunits, ( self._nbins - 1)])
+        return tf.math.softplus(x) + 1e-2
+
+    def __call__(self, x, nunits):
+
+        bin_positions = self._bin_positions
+        if not self._built:
+            self._bin_widths  = tf.layers.Dense(self._nunits * self._nbins, activation=bin_positions, name='w')
+            self._bin_heights = tf.layers.Dense(self._nunits * self._nbins, activation=bin_positions, name='h')
+            self._knot_slopes = tf.layers.Dense(self._nunits * (self._nbins - 1), activation=self._slopes, name='s')
+        self._built = True
+        result = tfb.RationalQuadraticSpline(bin_widths=self._bin_widths(x),bin_heights=self._bin_heights(x),knot_slopes=self._knot_slopes(x))
+        
+        return result
+
+
+
+
+def trainable_lu_factorization(
+    event_size, batch_shape=(), seed=None, dtype=tf.float32, name=None):
+    with tf.name_scope(name or 'trainable_lu_factorization'):
+        event_size = tf.convert_to_tensor(
+            event_size, dtype_hint=tf.int32, name='event_size')
+        batch_shape = tf.convert_to_tensor(
+            batch_shape, dtype_hint=event_size.dtype, name='batch_shape')
+        random_matrix = tf.random.uniform(
+            shape=tf.concat([batch_shape, [event_size, event_size]], axis=0),
+            dtype=dtype,
+            seed=seed)
+        random_orthonormal = tf.linalg.qr(random_matrix)[0]
+        lower_upper, permutation = tf.linalg.lu(random_orthonormal)
+        lower_upper = tf.Variable(
+            initial_value=lower_upper,
+            trainable=True,
+            name='lower_upper')
+        permutation = tf.Variable(
+            initial_value=permutation,
+            trainable=False,
+            name='permutation')
+        return lower_upper, permutation
+
+
+
+def get_nvp(params):
+    base_dis          = get_prior(params['latent_size'])
+    chain             = []
+    reduction         = (params['latent_size']-params['flow_dims'])/params['latent_size']
+    current_size      = []
+    perms_swap        = []
+    perms_rand        = []
+    perms_train       = []
+    splines           = []
+    size              = []
+    for i,s in enumerate(params['flow_size']):
+        current_size  = np.int(params['latent_size']*(1-reduction[i]))
+        swapping      = np.concatenate((np.arange(current_size//2,current_size),np.arange(0, current_size//2)))
+        perms_swap.append(tfb.Permute(permutation=init_once(swapping,name="perm_swap%d"%i)))
+        permutation   = np.random.permutation(np.arange(0, current_size))
+        perms_rand.append(tfb.Permute(permutation=init_once(permutation,name="perm_rand%d"%i)))
+        perms_train.append(tfb.MatvecLU(*trainable_lu_factorization(current_size),validate_args=True, name="perms_train%d"%i))
+        splines.append(SplineParams(current_size//2))
+        size.append(current_size)
+
+    chain.append(tfb.Scale(params['scale']))
+    for i,s in enumerate(params['flow_size']):
+        if i%2==0:
+            chain.append(tfb.Blockwise(bijectors=[perms_train[i],tfb.Identity()],block_sizes=[size[i],params['latent_size']-size[i]]))
+        else:
+            chain.append(tfb.Blockwise(bijectors=[perms_swap[i],tfb.Identity()],block_sizes=[size[i],params['latent_size']-size[i]]))
+        if i<num_nsf: 
+            bijector1 = tfb.RealNVP(num_masked=size[i]-size[i]//2,bijector_fn=splines[i])
+        if i<num_nsf+num_nvp and i>num_nsf:
+            bijector1 = tfb.RealNVP(num_masked=size[i]-size[i]//2,shift_and_log_scale_fn=tfb.real_nvp_default_template(hidden_layers=[params['latent_size'],params['latent_size']]))                 
+        else:
+            bijector1 = tfb.RealNVP(num_masked=size[i]-size[i]//2,shift_and_log_scale_fn=tfb.real_nvp_default_template(hidden_layers=[params['latent_size'],params['latent_size']],shift_only=True))  
+        bijector2 = tfb.Identity()
+        chain.append(tfb.Blockwise(bijectors=[bijector1, bijector2], block_sizes=[size[i],params['latent_size']-size[i]]))
+    chain.append(tfb.Scale(tf.cast(1/params['scale'], tf.float32)))
+
+    # p(x) is Gaussian, chain: y=g(x)
+    nvp=tfd.TransformedDistribution(distribution=tfd.Normal(0,1),bijector=tfb.Chain(chain),name='mynvp', event_shape=(params['latent_size'],))
+
+    return nvp
+
+
+def make_nvp(params):
+
+    def create_nvp_specs():
+        z_sample     = tf.placeholder(tf.float32, shape=[None,params['latent_size']])
+        sample_size  = tf.placeholder(tf.int32, shape=[])
+        u_sample     = tf.placeholder(tf.float32, shape=[None,params['latent_size']])
+        nvp_         = get_nvp(params)
+        log_prob     = nvp_.log_prob(z_sample)
+        nvp_sample   = nvp_.sample(sample_size)
+        nvp_fwd_pass = nvp_.bijector.forward(u_sample)
+        nvp_bwd_pass = nvp_.bijector.inverse(z_sample)
+        hub.add_signature(inputs={'z_sample':z_sample,'sample_size':sample_size, 'u_sample':u_sample} ,outputs={'log_prob':log_prob, 'sample':nvp_sample, 'fwd_pass': nvp_fwd_pass, 'bwd_pass': nvp_bwd_pass})
+
+    nvp_spec = hub.create_module_spec(create_nvp_specs)
+    nvp      = hub.Module(nvp_spec, name='flow', trainable=True)
+
+    hub.register_for_export(nvp, 'flow')
+
+    return nvp
